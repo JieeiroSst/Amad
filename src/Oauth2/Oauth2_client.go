25948da1540c3d9650b6ff79dc3731958ea750c5
Alan luu <luumanhquan.91@gmail.com>
2020-03-03 03:56:24 +0700

    add file images

--- src/Oauth2/Oauth2_client.go
+++ src/Oauth2/Oauth2_client.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"golang.org/x/oauth2"
@@ -61,18 +60,18 @@ func Authorize(w http.ResponseWriter, r *http.Request) {
 
 //func main() {
 
-	// 1 - We attempt to hit our Homepage route
-	// if we attempt to hit this unauthenticated, it
-	// will automatically redirect to our Auth
-	// server and prompt for login credentials
-	//http.HandleFunc("/", HomePage)
+// 1 - We attempt to hit our Homepage route
+// if we attempt to hit this unauthenticated, it
+// will automatically redirect to our Auth
+// server and prompt for login credentials
+//http.HandleFunc("/", HomePage)
 
-	// 2 - This displays our state, code and
-	// token and expiry time that we get back
-	// from our Authorization server
-	//http.HandleFunc("/oauth2", Authorize)
+// 2 - This displays our state, code and
+// token and expiry time that we get back
+// from our Authorization server
+//http.HandleFunc("/oauth2", Authorize)
 
-	// 3 - We start up our Client on port 9094
-	//log.Println("Client is running at 9094 port.")
-	//log.Fatal(http.ListenAndServe(":9094", nil))
+// 3 - We start up our Client on port 9094
+//log.Println("Client is running at 9094 port.")
+//log.Fatal(http.ListenAndServe(":9094", nil))
 //}
