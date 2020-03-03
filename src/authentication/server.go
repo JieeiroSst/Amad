ba316bb0ccfa22fe85cdaedcb092c5b1d85b202b
Alan luu <luumanhquan.91@gmail.com>
2020-03-03 17:53:23 +0700

    authcation

---
+++ src/authentication/server.go
@@ -0,0 +1,48 @@
+package authentication
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+//var mySigningKey = []byte("captainjacksparrowsayshi")
+
+func homePages(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+	fmt.Println("Endpoint Hit: homePage")
+
+}
+
+func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header["Token"] != nil {
+
+			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+				}
+				return mySigningKey, nil
+			})
+
+			if err != nil {
+				fmt.Fprintf(w, err.Error())
+			}
+
+			if token.Valid {
+				endpoint(w, r)
+			}
+		} else {
+
+			fmt.Fprintf(w, "Not Authorized")
+		}
+	})
+}
+
+func handleRequestss() {
+	http.Handle("/", isAuthorized(homePage))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
